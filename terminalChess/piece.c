044e8915d13f349cf6f3fef4346907685470c1cd
TimothySanchirico <timothy.sanchirico@gmail.com>
2016-04-18 21:29:57 -0500

    full piece move shit

--- terminalChess/piece.c
+++ terminalChess/piece.c
@@ -75,7 +75,18 @@ int space_occupied(int x, int y, piece * team){
 int type_can_move(piece *** b, int team, unsigned int type, unsigned int start_x, unsigned int start_y, unsigned int end_x, unsigned int end_y){
 	switch(type){
 		case(PAWN):
-			return pawn_can_move(b, team, type, start_x, start_y, end_x, end_y);	
+			return pawn_can_move(b, team, type, start_x, start_y, end_x, end_y);
+		case(BISHOP):
+			return bishop_can_move(b, team, type, start_x, start_y, end_x, end_y);
+		case(ROOK):
+			return rook_can_move(b, team, type, start_x, start_y, end_x, end_y);
+		case(KNIGHT):
+			return knight_can_move(b, team, type, start_x, start_y, end_x, end_y);
+		case(QUEEN):
+			return queen_can_move(b, team, type, start_x, start_y, end_x, end_y);
+		case(KING):
+			return king_can_move(b, team, type, start_x, start_y, end_x, end_y);
+				
 
 	}
 
@@ -105,14 +116,16 @@ int pawn_can_move(piece *** b, int team, unsigned int type, unsigned int start_x
 			if(end_y == (team == WHITE ? start_y - 1 : start_y + 1) ){
 				if(end_x == start_x + 1 || end_x == start_x - 1)
 					return 1; 
+				else return 0;
 			}
+			else return 0;
 		}
 		else
 			return 0;
 	}
 					
 	/* Make sure the space isnt occupied */
-	if( (end_y == (team == WHITE ? start_y - 1 : start_y + 1) ) && (start_x == end_x) )
+	else if( (end_y == (team == WHITE ? start_y - 1 : start_y + 1) ) && (start_x == end_x) )
 		return 1;
 			
 	/* If we're still at the start row we can move two */
@@ -126,23 +139,137 @@ int pawn_can_move(piece *** b, int team, unsigned int type, unsigned int start_x
 
 int bishop_can_move(piece *** b, int team, unsigned int type, unsigned int start_x, unsigned int start_y,
 									 unsigned int end_x, unsigned int end_y){
+	return check_diagnol(b, start_x, start_y, end_x, end_y);
+}
+
+int rook_can_move(piece *** b, int team, unsigned int type, unsigned int start_x, unsigned int start_y,
+									 unsigned int end_x, unsigned int end_y){
+	return check_horizontal(b, start_x, start_y, end_x, end_y) || check_vertical(b, start_x, start_y, end_x, end_y);
+
+}
+
+
+int queen_can_move(piece *** b, int team, unsigned int type, unsigned int start_x, unsigned int start_y,
+									 unsigned int end_x, unsigned int end_y){
+	return check_horizontal(b, start_x, start_y, end_x, end_y) || check_vertical(b, start_x, start_y, end_x, end_y) ||
+		check_diagnol(b, start_x, start_y, end_x, end_y);
+
+}
+
+int king_can_move(piece *** b, int team, unsigned int type, unsigned int start_x, unsigned int start_y,
+										 unsigned int end_x, unsigned int end_y){
+	if( ( (start_x == end_x + 1) || (start_x == end_x -1) || (start_x == end_x) ) && 
+			( (start_y == end_y + 1) || (start_y == end_y - 1 ) || (start_y == end_y)) )
+		return queen_can_move(b, team, type, start_x, start_y, end_x, end_y);
+		 
+	else return 0;
+
+}
+
+
+int knight_can_move(piece *** b, int team, unsigned int type, unsigned int start_x, unsigned int start_y,
+									 unsigned int end_x, unsigned int end_y){
+	if(start_x == end_x + 2) {
+		y_checks:
+		if(start_y == end_y +1) return 1;
+		else if(start_y == end_y -1) return 1;
+	}
+	else if(start_x == end_x -2){
+		goto y_checks;
+	}
+	else if(start_y == end_y + 2){
+		x_checks:
+		if(start_x == end_x +1) return 1;
+		else if(start_x == end_x -1) return 1;
+	}
+	else if(start_y == end_y + 2){
+		goto x_checks;
+	}
+	return 0;
 
 }
 
 
 /* Make sure there are no pieces in the way on the diagnol */
-int check_diagnol(unsigned int start_x, unsigned int start_y, unsigned int end_x, unsigned int end_y){
+int check_diagnol(piece *** b, unsigned int start_x, unsigned int start_y, unsigned int end_x, unsigned int end_y){
+	/* Establish Direction */
+	int step = 1;
+	if(start_x > end_x){
+		if(start_y > end_y){
+			/* Down and to the right */
+			if(start_x - end_x != start_y - end_y) return 0;
+			do{
+				start_x -= step;
+				start_y -= step;
 
+				if(b[start_x][start_y] != NULL) return 0;
+
+			}while(start_x != end_x + 1);				
+		}
+		else {
+			/* Up and to the right */
+			if(start_x - end_x != end_y - start_y) return 0;
+			do{
+				start_x -= step;
+				start_y += step;
+
+				if(b[start_x][start_y] != NULL) return 0;
+
+			}while(start_x != end_x + 1);				
+		}
+	}
+	else if(start_y > end_y){
+		/* Down and to the left */	
+			if(end_x - start_x != start_y - end_y) return 0;
+			do{
+				start_x += step;
+				start_y -= step;
+
+				if(b[start_x][start_y] != NULL) return 0;
+
+			}while(start_x != end_x - 1);				
+	}
+	else {
+		/*Up and to the left*/
+			if(end_x - start_x != end_y - start_y) return 0;
+			do{
+				start_x += step;
+				start_y += step;
+
+				if(b[start_x][start_y] != NULL) return 0;
+
+			}while(start_x != end_x - 1);				
+	}
+	return 1;	
 }
 
 /* Checks all the spaces along the horizontal */
-int check_horizontal(unsigned int start_x, unsigned int start_y, unsigned int end_x, unsigned int end_y){
+int check_vertical(piece *** b, unsigned int start_x, unsigned int start_y, unsigned int end_x, unsigned int end_y){
+	if(start_x != end_x) return 0;
 
+	int step = (start_y > end_y  ? -1 : 1);
+
+	while( (start_y + step) != end_y){
+		start_y += step;
+		if(b[start_x][start_y] != NULL) return 0;
+	}
+	
+	return 1;
 }
 
 /* Checks all spaces along the vertical */
-int check_vertical(unsigned int start_x, unsigned int start_y, unsigned int end_x, unsigned int end_y){
+int check_horizontal(piece *** b,unsigned int start_x, unsigned int start_y, unsigned int end_x, unsigned int end_y){
+	if(start_y != end_y) return 0;
 
+	int step = (start_x > end_x  ? -1 : 1);
+
+	while( (start_x + step) != end_x){
+		start_x += step;
+		if(b[start_x][start_y] != NULL) return 0;
+	}
+	
+	return 1;
+	
 }
 
 
